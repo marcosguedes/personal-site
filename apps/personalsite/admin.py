85f77d34223856f85c932d21982ec11054b0e344
Marcos Guedes <marcos@nospampls.com.pt>
2019-01-01 18:41:44 +0000

    forgot to add About page in the admin

--- apps/personalsite/admin.py
+++ apps/personalsite/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import HomePage
+from .models import HomePage, AboutPage
 from solo.admin import SingletonModelAdmin
 
 
 admin.site.register(HomePage, SingletonModelAdmin)
+admin.site.register(AboutPage, SingletonModelAdmin)
